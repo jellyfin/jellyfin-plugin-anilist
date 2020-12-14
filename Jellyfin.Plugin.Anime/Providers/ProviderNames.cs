9b9af258b602e49344529f716e2b0313da6cda87
Odd Stråbø <oddstr13@openshell.no>
2020-12-14 23:48:40 +0100

    Remove other providers

--- Jellyfin.Plugin.Anime/Providers/ProviderNames.cs
+++ Jellyfin.Plugin.Anime/Providers/ProviderNames.cs
@@ -2,9 +2,6 @@
 {
     public class ProviderNames
     {
-        public const string AniDb = "AniDB";
         public const string AniList = "AniList";
-        public const string AniSearch = "AniSearch";
-        public const string KitsuIo = "Kitsu";
     }
 }
