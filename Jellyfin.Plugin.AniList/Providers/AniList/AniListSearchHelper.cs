1882f990293b8ca99e8c01eaefba528c7dda8c1e
Duck, Scrooge Duck <59000716+MisterMcDuck@users.noreply.github.com>
2024-04-12 22:42:50 +0000

    Tweaked AnilistSearchHelper.PreprocessTitle() to handle more search failures (#49)

--- Jellyfin.Plugin.AniList/Providers/AniList/AniListSearchHelper.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListSearchHelper.cs
@@ -13,20 +13,29 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
         { //Remove items that will always cause anilist to fail
             String input = path;
 
-            //Season designation 
+            //Season designation
             input = Regex.Replace(
-                input, 
+                input,
                 @"(\s|\.)S[0-9]{1,2}", String.Empty);
             // ~ ALT NAME ~
             input = Regex.Replace(
-                input, 
+                input,
                 @"\s*~(\w|[0-9]|\s)+~", String.Empty);
 
             // Native Name (English Name)
             // Only replaces if the name ends with a parenthesis to hopefully avoid mangling titles with parens (e.g. Evangelion 1.11 You Are (Not) Alone)
             input = Regex.Replace(
-                input.Trim(), 
+                input.Trim(),
                 @"\((\w|[0-9]|\s)+\)$", String.Empty);
+
+            // Replace & with "and" to avoid lookup failures
+            input = Regex.Replace(input, @"\s?&\s?", " and ");
+
+            // Replace the following characters with a space, to avoid failed lookups
+            input = Regex.Replace(input, @"\#", " ");
+
+            // Truncate suggested Jellyfin folder format for the anilist search. Example: The Melancholy of Haruhi Suzumiya (2006) [tvdbid-79414]
+            input = Regex.Replace(input.Trim(), @"\([0-9]{4}\)\s*\[(\w|[0-9]|-)+\]$", String.Empty);
 
 
             return input;
