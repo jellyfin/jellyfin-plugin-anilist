71849a74c151bc8cd96087907ed65d1bc31afc04
Shivesh M M <mmshivesh@gmail.com>
2024-04-27 21:41:44 -0400

    Decouple Language filter from Title preference - This change allows people to choose any combination of TitleLanguage and originalTitleLanguage, while still filtering by Localized/JP only People list

--- Jellyfin.Plugin.AniList/Providers/AniList/ApiModel.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/ApiModel.cs
@@ -198,11 +198,11 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
             {
                 foreach (VoiceActor va in edge.voiceActors)
                 {
-                    if (config.FilterPeopleByTitlePreference) {
-                        if (config.TitlePreference != TitlePreferenceType.Localized && va.language != "JAPANESE") {
+                    if (config.PersonLanguageFilterPreference != LanguageFilterType.All) {
+                        if (config.PersonLanguageFilterPreference == LanguageFilterType.Japanese && va.language != "JAPANESE") {
                             continue;
                         }
-                        if (config.TitlePreference == TitlePreferenceType.Localized && va.language == "JAPANESE") {
+                        if (config.PersonLanguageFilterPreference == LanguageFilterType.Localized && va.language == "JAPANESE") {
                             continue;
                         }
                     }
